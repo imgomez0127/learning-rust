4f6b902362828597cd1d36211a63c249a6bc341d
imgomez0127 <ianm0127@gmail.com>
2019-08-07 16:49:34 -0400

    Testing the difference between expressions and statements in rust works pretty similar to other functional languages where they define expression to always be evaluatable also {} work similar to begin end in ocaml where you chain a bunch of statements (in ocaml it would all eval to unitval) and then return the last expressions evaluation

--- hello_world/hello-world.rs
+++ hello_world/hello-world.rs
@@ -1,3 +1,11 @@
+fn test_fun() -> i32{
+    6
+}
 fn main(){
     println!("Hello World!");
+    let x = 0.1;
+    let y = 1;
+    let z = test_fun();
+    println!("{} {} ",x,y);
+    println!("{}",z);
 }
